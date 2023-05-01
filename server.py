60b5ad36e4a604a4bbf0e8e73054ee76699a1fd2
Artem Pospeev <pospeev.artem@icloud.com>
2023-05-01 19:30:38 +0400

    added logger and code refactoring

--- server.py
+++ server.py
@@ -1,39 +1,94 @@
-import json
+"""Программа-сервер"""
+
 import socket
+import sys
+import argparse
+import json
+import logging
+import logs.config_server_log
+from errors import IncorrectDataRecivedError
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, DEFAULT_PORT, MAX_CONNECTIONS, ERROR
+from common.utils import get_message, send_message
 
-from settings import DEFAULT_PORT, DEFAULT_IP_ADDRESS, ENCODING, MAX_PACKAGE_LENGTH, MAX_CONNECTION_COUNT
-from utils import Statuses, send_message, get_args
+#Инициализация логирования сервера.
+SERVER_LOGGER = logging.getLogger('server')
+
+def process_client_message(message):
+    """
+    Обработчик сообщений от клиентов, принимает словарь - сообщение от клинта,
+    проверяет корректность, возвращает словарь-ответ для клиента
+    :param message:
+    :return:
+    """
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and \
+            USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        return {RESPONSE: 200}
+    return {
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
+    }
 
 
-def handle_request(client_socket, request):
-    # Обработка запроса от клиента
-    response = {}  # Инициализация ответа
-    if request["action"] == "presence":
-        response["response"] = str(Statuses.OK)
-    else:
-        response["response"] = str(Statuses.BAD_REQUEST)
-    send_message(client_socket, response)
-
-
-def start_server(host=DEFAULT_IP_ADDRESS, port=DEFAULT_PORT):
-    # Запуск сервера
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
-    server_socket.listen(MAX_CONNECTION_COUNT)
-
-    while True:
-        client_socket, address = server_socket.accept()
-        data = client_socket.recv(MAX_PACKAGE_LENGTH).decode(ENCODING)
-        request = json.loads(data)
-        handle_request(client_socket, request)
-        client_socket.close()
+def create_arg_parser():
+    """
+    Парсер аргументов коммандной строки
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
 
 
 def main():
-    args = get_args(is_server=True)
+    """
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию
+    :return:
+    """
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
 
-    start_server(args.addr, args.port)
+    # проверка получения корретного номера порта для работы сервера.
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                               f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+    # Готовим сокет
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.bind((listen_address, listen_port))
+
+    # Слушаем порт
+
+    transport.listen(MAX_CONNECTIONS)
+
+    while True:
+        client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+        try:
+            message_from_cient = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_cient}')
+            response = process_client_message(message_from_cient)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
+            send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            client.close()
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+                                f'клиента {client_address}. Соединение закрывается.')
+            client.close()
+        except IncorrectDataRecivedError:
+            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
+                                f'Соединение закрывается.')
+            client.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
